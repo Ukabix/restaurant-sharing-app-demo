735daa394372dfa922138205ee59d74922cfa702
Ukabix <54454097+Ukabix@users.noreply.github.com>
2021-11-22 00:57:07 +0100

    serving html sites, mdir views, migrate htmls

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
+// import path
+const path = require("path");
 // import express
 const express = require("express");
 // make an app
 const app = express();
-// setup dummy path
-app.get("/", function(req, res) {
-  res.send("<h1>Hello World</h1>");
-});
+// serve dummy page
+// app.get("/", function(req, res) {
+//   res.send("<h1>Hello World</h1>");
+// });
 
+//// register routes
+// serve /index
+app.get("/", function (req, res){
+  // construct a path
+  const htmlFilePath = path.join(__dirname, "views", "index.html");
+  // send 
+  res.sendFile(htmlFilePath);
+});
+// serve /restaurants
+app.get("/restaurants", function (req, res){
+  // construct a path
+  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
+  // send 
+  res.sendFile(htmlFilePath);
+});
+// serve /recommend
+app.get("/recommend", function (req, res){
+  // construct a path
+  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+  // send 
+  res.sendFile(htmlFilePath);
+});
+// serve /confirm
+app.get("/confirm", function (req, res){
+  // construct a path
+  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
+  // send 
+  res.sendFile(htmlFilePath);
+});
+// serve /about
+app.get("/about", function (req, res){
+  // construct a path
+  const htmlFilePath = path.join(__dirname, "views", "about.html");
+  // send 
+  res.sendFile(htmlFilePath);
+});
 
 
 // setup a server with listen
