09953fd41b0b593a1e92b3e3677b04b23807b53b
Ukabix <54454097+Ukabix@users.noreply.github.com>
2021-11-22 02:26:05 +0100

    parse form data and redirect request

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// import filesystem
+const fs = require("fs");
 // import path
 const path = require("path");
 // import express
@@ -9,6 +11,9 @@ const app = express();
 // for each incoming request express will check the public folder and will return it
 // if not it will reroute to next handlers
 app.use(express.static("public"));
+
+// middleware for parsing incoming post data
+app.use(express.urlencoded({extended: false}));
 
 // serve dummy page
 // app.get("/", function(req, res) {
@@ -23,6 +28,7 @@ app.get("/", function (req, res){
   // send 
   res.sendFile(htmlFilePath);
 });
+
 // serve /restaurants
 app.get("/restaurants", function (req, res){
   // construct a path
@@ -30,6 +36,7 @@ app.get("/restaurants", function (req, res){
   // send 
   res.sendFile(htmlFilePath);
 });
+
 // serve /recommend
 app.get("/recommend", function (req, res){
   // construct a path
@@ -37,6 +44,28 @@ app.get("/recommend", function (req, res){
   // send 
   res.sendFile(htmlFilePath);
 });
+// for user input - make post route handlers
+app.post("/recommend", function(req, res) {
+  // extract data - store body!
+  const restaurant = req.body;
+  //// create JSON file - /data/restaurants.json
+  /// open that file, write data and store it back
+  // get filepath to restaurants
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  // open and read file
+  const fileData = fs.readFileSync(filePath);
+  // translate json to array with parser
+  const storedRestaurants = JSON.parse(fileData);
+  // add new data to array
+  storedRestaurants.push(restaurant);
+  // save the array as JSON with stringify
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+  /// send response
+  // redirect user
+  res.redirect("/confirm"); // reconfigure html after that
+
+});
+
 // serve /confirm
 app.get("/confirm", function (req, res){
   // construct a path
@@ -44,6 +73,7 @@ app.get("/confirm", function (req, res){
   // send 
   res.sendFile(htmlFilePath);
 });
+
 // serve /about
 app.get("/about", function (req, res){
   // construct a path
@@ -51,6 +81,9 @@ app.get("/about", function (req, res){
   // send 
   res.sendFile(htmlFilePath);
 });
+
+//// handling form 
+
 
 
 // setup a server with listen
