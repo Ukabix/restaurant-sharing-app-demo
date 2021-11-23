651da98eb69dd25a2c3222bdf403e95b3d20a92c
Ukabix <54454097+Ukabix@users.noreply.github.com>
2021-11-23 02:39:17 +0100

    export restaurant app routes to restaurants.js + add imports + module.exports

--- app.js
+++ app.js
@@ -41,65 +41,7 @@ app.use("/", defaultRoutes);
 
 //// register routes
 
-// serve /restaurants
-app.get("/restaurants", function (req, res) {
-  // call resData funcs
-  const storedRestaurants = resData.getStoredRestaurants();
 
-  /// use render method for ejs, 2nd argument is for dynamic elements in ejs
-  res.render("restaurants", {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
-});
-// define path for dynamic routes via colon
-app.get("/restaurants/:id", function(req, res) {
-  // get access to concrete value for id
-  const restaurantId = req.params.id;
-  // call resData 
-  const storedRestaurants = resData.getStoredRestaurants();
-
-  /// find restaurant in array - with for of loop
-  for (const restaurant of storedRestaurants) {
-    // compare restaurant.id with restaurantId
-    if (restaurant.id === restaurantId) {
-        // render new view with accepted dynamic argument { rid: }
-        // pass restaurant
-        return res.render("restaurant-detail", { restaurant: restaurant });
-    }
-  }
-  // at this point we know there is no matchning id
-  res.render("404");
-});
-
-
-// serve /recommend
-app.get("/recommend", function (req, res) {
-  // use render method for ejs
-  res.render("recommend");
-});
-// for user input - make post route handlers
-app.post("/recommend", function (req, res) {
-  /// extract data - store body!
-  const restaurant = req.body;
-  // enrich body with uuid -> new property via uuid
-  restaurant.id = uuid.v4();
-  // get getStoredRestaurants from restaurant-data.js
-  const restaurants = resData.getStoredRestaurants();
-  // add new data to array
-  restaurants.push(restaurant);
-  // call storeRestaurants from resData
-  resData.storeRestaurants(restaurants);
-  /// send response
-  // redirect user
-  res.redirect("/confirm"); // reconfigure html after that
-});
-
-// serve /confirm
-app.get("/confirm", function (req, res) {
-  // use render method for ejs
-  res.render("confirm");
-});
 
 
 // middleware for 404 - will catch any requests that are not handled via other routes
