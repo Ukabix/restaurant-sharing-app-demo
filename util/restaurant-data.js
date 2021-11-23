28e9788e48043fa6c178eea45073a7c26adf5933
Ukabix <54454097+Ukabix@users.noreply.github.com>
2021-11-23 02:20:40 +0100

    fixed path in restaurant-data.js with ".."

--- util/restaurant-data.js
+++ util/restaurant-data.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 // get filepath to restaurants
-const filePath = path.join(__dirname, "data", "restaurants.json");
+const filePath = path.join(__dirname, "..", "data", "restaurants.json");
 
 function getStoredRestaurants() {
   //// create JSON file - /data/restaurants.json
